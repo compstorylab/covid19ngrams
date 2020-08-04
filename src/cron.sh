52b2c2b129a6d1ea7ed2ae3cf4de322736a4c652
Thayer Alshaabi <thayeralshaabi@gmail.com>
2020-08-04 12:41:22 -0400

    REF update cron

--- src/cron.sh
+++ src/cron.sh
@@ -4,4 +4,5 @@
 ~/anaconda3/envs/storywrangler/bin/python -u ~/www-root/share/data/covid19ngrams/src/update.py &> ~/www-root/share/data/covid19ngrams/data/log.txt
 
 # udpate figures for https://arxiv.org/abs/2003.12614
-~/anaconda3/envs/storywrangler/bin/python -u ~/www-root/share/data/covid19ngrams/src/update.py figures &>> ~/www-root/share/data/covid19ngrams/data/log.txt
+git -C ~/www-root/share/data/COVID-19 pull
+~/anaconda3/envs/storywrangler/bin/python -u ~/www-root/share/data/covid19ngrams/src/update.py figures ~/www-root/share/data/COVID-19 &>> ~/www-root/share/data/covid19ngrams/data/log.txt
