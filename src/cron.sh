26fdf696ff8e79676d47978c0192c552455679f9
Thayer Alshaabi <thayeralshaabi@gmail.com>
2020-08-06 11:48:11 -0400

    FIX cron

--- src/cron.sh
+++ src/cron.sh
@@ -3,15 +3,13 @@
 repo="$HOME/www-root/share/data/covid19ngrams"
 script="$repo/src/update.py"
 log="$repo/data/log.txt"
-profiler="/usr/bin/time -v -o $repo/data/resources.txt"
-env="$HOME/anaconda3/envs/storywrangler/bin/python"
 jhu="$HOME/www-root/share/data/COVID-19"
 
 
 # update n-grams timeseries
-"$profiler $env -u $script &> $log"
+/usr/bin/time -v -o $repo/data/resources.txt ~/anaconda3/envs/storywrangler/bin/python $script &> $log
 
 
 # udpate figures for https://arxiv.org/abs/2003.12614
-git -C "$jhu" pull
-"$profiler $env -u $script figures $jhu &>> $log"
+git -C $jhu pull
+~/anaconda3/envs/storywrangler/bin/python $script figures $jhu &>> $log
